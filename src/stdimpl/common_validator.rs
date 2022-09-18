d010b87d080c64e8a04ff5dea955e986a8594f01
Alex Beaver <alex@abvr.xyz>
2022-09-18 02:04:26 -0400

    Work in writing policy validator

--- src/stdimpl/common_validator.rs
+++ src/stdimpl/common_validator.rs
@@ -1,5 +1,6 @@
 use crate::supporting::datastore::hivemind::Hivemind;
-use crate::supporting::policy::powerpolicy::{PolicyValidator, PolicyValidatorResponse};
+use crate::supporting::policy::powerpolicy::{PolicyTemplate, PolicyValidator, PolicyValidatorResponse};
+use crate::supporting::policy::powerpolicy::PolicyValidatorVote::{Abstain, Invalid, Valid};
 use crate::supporting::trust::certmgr::SignedCertificateRequest;
 
 pub struct TemplateValidator{
@@ -7,8 +8,28 @@ pub struct TemplateValidator{
 }
 impl PolicyValidator for TemplateValidator {
     fn validate<H: Hivemind>(&self, request: SignedCertificateRequest, hivemind: &H) -> PolicyValidatorResponse {
+        let template = hivemind.get(request.template.clone());
+        if template.is_none(){
+            return PolicyValidatorResponse{
+                vote: Abstain,
+                confidence: 0,
+            };
+        }
+        let template = template.unwrap();
+
+        let parsed_template: Err<PolicyTemplate> = serde_json::from_str(&template.as_str());
+        if parsed_template.is_err(){
+            return PolicyValidatorResponse{
+                vote: Invalid,
+                confidence: 1000,
+            };
+        }
+        let parsed_template = parsed_template.unwrap();
+        
+
+
         return PolicyValidatorResponse{
-            valid: true,
+            vote: Valid,
             confidence: 1000,
         };
     }
@@ -20,7 +41,7 @@ pub struct ChainOfTrustValidator{
 impl PolicyValidator for ChainOfTrustValidator {
     fn validate<H: Hivemind>(&self, request: SignedCertificateRequest, hivemind: &H) -> PolicyValidatorResponse {
         return PolicyValidatorResponse{
-            valid: true,
+            vote: Valid,
             confidence: 1000,
         };
     }
--- src/supporting/policy/powerpolicy.rs
+++ src/supporting/policy/powerpolicy.rs
@@ -1,11 +1,69 @@
+use core::fmt;
 use crate::supporting::datastore::hivemind::Hivemind;
 use crate::supporting::trust::certmgr::SignedCertificateRequest;
 
+/// The ways that a validator can vote on a specific request
+pub enum PolicyValidatorVote{
+    /// The validator believes that the request is valid
+    Valid,
+    /// The validator believes that the request is invalid
+    Invalid,
+    /// The validator does not want to make a decision on the request
+    Abstain,
+    /// The validator is unable to make a decision on the request
+    Unknown,
+}
+
+impl PolicyValidatorVote{
+    /// Convert a PolicyValidatorVote to a boolean
+    /// *true* if it is valid
+    /// *false* if it is not valid
+    pub fn to_bool(&self) -> bool{
+        match self{
+            PolicyValidatorVote::Valid => true,
+            PolicyValidatorVote::Invalid => false,
+            PolicyValidatorVote::Abstain => false,
+            PolicyValidatorVote::Unknown => false,
+        }
+    }
+}
+impl fmt::Display for PolicyValidatorVote {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            PolicyValidatorVote::Valid => write!(f, "VALID"),
+            PolicyValidatorVote::Invalid => write!(f, "INVALID"),
+            PolicyValidatorVote::Abstain => write!(f, "ABSTAIN"),
+            PolicyValidatorVote::Unknown => write!(f, "UNKNOWN"),
+        }
+    }
+}
+
 pub struct PolicyValidatorResponse{
-    pub valid: bool,
-    pub confidence: u32,
+    /// Whether the validators believes that the action is valid
+    pub vote: PolicyValidatorVote,
+
+    /// The magnitude of the confidence that the validator has in its decision
+    pub confidence: u32
 }
 pub trait PolicyValidator{
     fn validate<H:Hivemind>(&self, request: SignedCertificateRequest, hivemind: &H) -> PolicyValidatorResponse;
+
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct PolicyTemplate{
+
+}
+
+pub enum PolicyTemplateValidationMethod{
+    /// It must match direct string equality
+    Exact,
+    /// It must match a regex
+    Regex,
+    /// It must match based on security degree
+    /// Higher security degrees mean more secure/restricted
+    ///
+    /// **Note: Your string must be a valid u32.** Otherwise, the system will vote invalid.
+    SecurityDegree,
 
 }
